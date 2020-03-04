0e81b70109da9f4874bcccd7e9206307ea7c1c3a
mlaska2 <60201530+mlaska2@users.noreply.github.com>
2020-03-04 16:38:36 -0600

    cleaning up code
    indenting code to make it easier to read

--- js/main.js
+++ js/main.js
@@ -6,219 +6,223 @@ var dataStats = {};
 
 //function to instantiate the Leaflet Map
 function createMap() {
-  laskaMap = L.map("mapid", {
-    center: [39.83333, -98.58333],
-    zoom: 4
-  });
-  //add basemap tilelayer
-  // L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-	// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	// 	subdomains: 'abcd',
-	// 	maxZoom: 5
-  L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
-	  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-	  subdomains: 'abcd',
-	  maxZoom: 5
-	}).addTo(laskaMap);
-  //call getData function
-  getData(laskaMap);
+    laskaMap = L.map("mapid", {
+        center: [39.83333, -98.58333],
+        zoom: 4
+    });
+    //add basemap tilelayer
+    // L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
+  	// 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+  	// 	subdomains: 'abcd',
+  	// 	maxZoom: 5
+
+    L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+    	  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+    	  subdomains: 'abcd',
+    	  maxZoom: 5
+  	}).addTo(laskaMap);
+
+    //call getData function
+    getData(laskaMap);
 };
 
 //function to calculate max, min, and mean of the dataset from an array of all the values and assign them to the dataStats object
 function calcStats(data) {
-  //create empty array to store all data values
-  var allValues =[];
+    //create empty array to store all data values
+    var allValues =[];
 
-  //loop through each state
-  for (var state of data.features) {
-    //loop through each year
-    for (var year = 1990; year <= 2017; year+=1) {
-      //get million metric tons CO2 for current year
-      var value = state.properties["tons_"+ String(year)];
-      //add value to array
-      allValues.push(value);
+    //loop through each state
+    for (var state of data.features) {
+        //loop through each year
+        for (var year = 1990; year <= 2017; year+=1) {
+            //get million metric tons CO2 for current year
+            var value = state.properties["tons_"+ String(year)];
+            //add value to array
+            allValues.push(value);
+        }
     }
-  }
 
-  //get min, max, and mean stats for the array
-  dataStats.min = Math.min(...allValues);
-  dataStats.max = Math.max(...allValues);
+    //get min, max, and mean stats for the array
+    dataStats.min = Math.min(...allValues);
+    dataStats.max = Math.max(...allValues);
 
-  //calculate mean
-  var sum = allValues.reduce(function(a,b){return a+b;});
-  dataStats.mean = sum/allValues.length;
+    //calculate mean
+    var sum = allValues.reduce(function(a,b){return a+b;});
+    dataStats.mean = sum/allValues.length;
 };
 
 // function to calculate the radius of each prop symbol
 function calcPropRadius(attValue) {
-  //constant factor adjusts symbol sizes evenly
-  var minRadius=3
+    //constant factor adjusts symbol sizes evenly
+    var minRadius=3
 
-  //flannary appearance compensation formula
-  var radius = 1.0083*Math.pow(attValue/dataStats.min,0.5715)*minRadius
+    //flannary appearance compensation formula
+    var radius = 1.0083*Math.pow(attValue/dataStats.min,0.5715)*minRadius
 
-  return radius
+    return radius
 };
 
 //function to **********************************
 function pointToLayer(feature, latlng, attributes) {
-  //Step 4: Determine which attribute to visualize with prop symbols
-  var attribute = attributes[0];
+    //Step 4: Determine which attribute to visualize with prop symbols
+    var attribute = attributes[0];
 
-  //create marker options
-  var circleOptions = {
-    radius: 4,
-    fillColor: "#848484", //000000 because CO2 dirty, so visualize in black?
-		color: "#000",
-		weight: 1,
-		opacity: 1,
-		fillOpacity: 0.6
-  };
+    //create marker options
+    var circleOptions = {
+        radius: 4,
+        fillColor: "#848484", //000000 because CO2 dirty, so visualize in black?
+    		color: "#000",
+    		weight: 1,
+    		opacity: 1,
+    		fillOpacity: 0.6
+    };
 
-  //Step 5. For each feature, determine its value for the selected attribute
-  var attValue = Number(feature.properties[attribute]);
+    //Step 5. For each feature, determine its value for the selected attribute
+    var attValue = Number(feature.properties[attribute]);
 
-  //Step 6. Give each feature's circle marker a radius based on its attribute value
-  circleOptions.radius = calcPropRadius(attValue);
+    //Step 6. Give each feature's circle marker a radius based on its attribute value
+    circleOptions.radius = calcPropRadius(attValue);
 
-  //create circle marker layer
-  var circleLayer = L.circleMarker(latlng, circleOptions);
+    //create circle marker layer
+    var circleLayer = L.circleMarker(latlng, circleOptions);
 
-  //assign popupContent to the function that creates popup
-  var popupContent = createPopupContent(feature.properties, attribute);
+    //assign popupContent to the function that creates popup
+    var popupContent = createPopupContent(feature.properties, attribute);
 
-  //bind the popup to the circle marker
-  circleLayer.bindPopup(popupContent//, //the offset was displacing the popup based on where I clicked, it wasn;t just putting it at the top of the circle as the example made it seem
-    //{offset: new L.Point(0, circleOptions.radius)}
-  );
+    //bind the popup to the circle marker
+    circleLayer.bindPopup(popupContent//, //the offset was displacing the popup based on where I clicked, it wasn;t just putting it at the top of the circle as the example made it seem
+      //{offset: new L.Point(0, circleOptions.radius)}
+    );
 
-  //return the circle marker layer to the L.geoJson pointToLayer function
-  return circleLayer;
+    //return the circle marker layer to the L.geoJson pointToLayer function
+    return circleLayer;
 };
 
 //Step 3: Add circle markers for point features to the map
 function createPropSymbols(data, attributes) {
-  //create Leaflet geoJSON layer and add it to map
-  L.geoJson(data, {
-    pointToLayer: function(feature, latlng) {
-      return pointToLayer(feature, latlng, attributes);
-    }
-  }).addTo(laskaMap);
+    //create Leaflet geoJSON layer and add it to map
+    L.geoJson(data, {
+        pointToLayer: function(feature, latlng) {
+            return pointToLayer(feature, latlng, attributes);
+        }
+    }).addTo(laskaMap);
 };
 
 //function to create sequence slider and buttons
 function createSequenceControls(attributes) {
 
-  //create new sequence controls
-  var SequenceControl = L.Control.extend({
-    options: {
-      position: 'bottomleft'
-    },
+    //create new sequence controls
+    var SequenceControl = L.Control.extend({
+        options: {
+            position: 'bottomleft'
+        },
 
-    //onAdd method specifying a function to create DOM element, executed when control is added to the map
-    onAdd: function () {
-      //create control container div with a specified class name
-      var container = L.DomUtil.create('div', 'sequence-control-container');
+        //onAdd method specifying a function to create DOM element, executed when control is added to the map
+        onAdd: function () {
+            //create control container div with a specified class name
+            var container = L.DomUtil.create('div', 'sequence-control-container');
 
-      //intitalize other DOM elements if needed here
+            //intitalize other DOM elements if needed here
 
-      //create range input element (slider)
-      $(container).append('<input class="range-slider" type="range">');
+            //create range input element (slider)
+            $(container).append('<input class="range-slider" type="range">');
 
-      //create step buttons
-      $(container).append('<button class="step" id="reverse" title="Reverse">Reverse</button>');
-      $(container).append('<button class="step" id="forward" title="Forward">Forward</button>');
+            //create step buttons
+            $(container).append('<button class="step" id="reverse" title="Reverse">Reverse</button>');
+            $(container).append('<button class="step" id="forward" title="Forward">Forward</button>');
 
-      //disable mouse event listeners for the container
-      L.DomEvent.disableClickPropagation(container);
+            //disable mouse event listeners for the container
+            L.DomEvent.disableClickPropagation(container);
 
-      return container;
-    }
-  });
+            return container;
+        }
+    });
 
-  //add given control to the map
-  laskaMap.addControl(new SequenceControl());
+    //add given control to the map
+    laskaMap.addControl(new SequenceControl());
 
-  //NOW can set attributes/edit things within the control
+    //NOW can set attributes/edit things within the control
 
-  //set slider attributes
-  $('.range-slider').attr({
-    min:0,
-    max:27,
-    value:0,
-    step:1
-  });
+    //set slider attributes
+    $('.range-slider').attr({
+        min:0,
+        max:27,
+        value:0,
+        step:1
+    });
 
-  //replace button content with images
-  $('#reverse').html('<img src="img/leftArrow.png">');
-  $('#forward').html('<img src="img/rightArrow.png">');
+    //replace button content with images
+    $('#reverse').html('<img src="img/leftArrow.png">');
+    $('#forward').html('<img src="img/rightArrow.png">');
 
-  //click listener for buttons
-  $('.step').click(function() {
-    //get the old index value
-    var index = $('.range-slider').val();
+    //click listener for buttons
+    $('.step').click(function() {
+        //get the old index value
+        var index = $('.range-slider').val();
 
-    //increment or decrement depending on button clicked
-    if ($(this).attr('id') == 'forward') {
-        index++;
-        //if past the last attribute, wrap around to first attribute
-        index = index > 27 ? 0 : index;
-    } else if ($(this).attr('id') == 'reverse') {
-        index--;
-        //if past the first attribute, wrap around to last attribute
-        index = index < 0 ? 27 : index;
-    };
+        //increment or decrement depending on button clicked
+        if ($(this).attr('id') == 'forward') {
+            index++;
+            //if past the last attribute, wrap around to first attribute
+            index = index > 27 ? 0 : index;
+        } else if ($(this).attr('id') == 'reverse') {
+            index--;
+            //if past the first attribute, wrap around to last attribute
+            index = index < 0 ? 27 : index;
+        };
 
-    //update slider
-    $('.range-slider').val(index);
-    //console.log(index)
-    //pass new attribute to update symbols
-    updatePropSymbols(attributes[index]);
-  });
+        //update slider
+        $('.range-slider').val(index);
 
-  //input listener for slider
-  $('.range-slider').on('input', function() {
-    //get the new index value
-    var index = $(this).val();
-    //console.log(index)
-    //pass new attribute to update symbols
-    updatePropSymbols(attributes[index]);
-  });
+        //pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
+
+    //input listener for slider
+    $('.range-slider').on('input', function() {
+        //get the new index value
+        var index = $(this).val();
+
+        //console.log(index)
+
+        //pass new attribute to update symbols
+        updatePropSymbols(attributes[index]);
+    });
 };
 
 //function to create popup and eliminate redundancies in other functions
 function createPopupContent(properties, attribute) {
-  //add city to popup content string
-  var popupContent = "<p><b>State:</b> " + properties.State + "</p>";
+    //add city to popup content string
+    var popupContent = "<p><b>State:</b> " + properties.State + "</p>";
 
-  //add formatted attribute to panel content string
-  var year = attribute.split("_")[1];
-  popupContent += "<p><b>CO2 Emissions in " + year + ":</b> " + properties[attribute] + " million metric tons</p>";
+    //add formatted attribute to panel content string
+    var year = attribute.split("_")[1];
+    popupContent += "<p><b>CO2 Emissions in " + year + ":</b> " + properties[attribute] + " million metric tons</p>";
 
-  return popupContent;
+    return popupContent;
 };
 
 //resizse the prop symbols according to new attribute values
 function updatePropSymbols(attribute) {
-  laskaMap.eachLayer(function(layer) {
-    if (layer.feature && layer.feature.properties[attribute]) {
-      //access feature properties
-      var props = layer.feature.properties;
+    laskaMap.eachLayer(function(layer) {
+        if (layer.feature && layer.feature.properties[attribute]) {
+            //access feature properties
+            var props = layer.feature.properties;
 
-      //update each feature's radius based on new attribute values
-      var radius = calcPropRadius(props[attribute]);
-      layer.setRadius(radius);
+            //update each feature's radius based on new attribute values
+            var radius = calcPropRadius(props[attribute]);
+            layer.setRadius(radius);
 
-      //assign popupContent to the function that creates popup
-      var popupContent = createPopupContent(props, attribute);
+            //assign popupContent to the function that creates popup
+            var popupContent = createPopupContent(props, attribute);
 
-      //update popup content
-      popup=layer.getPopup();
-      popup.setContent(popupContent).update();
+            //update popup content
+            popup=layer.getPopup();
+            popup.setContent(popupContent).update();
 
-      $('.years').html('CO2 Emissions in ' + attribute.split("_")[1])
-    };
-  });
+            $('.years').html('CO2 Emissions in ' + attribute.split("_")[1])
+        };
+    });
 };
 
 //work in progress
